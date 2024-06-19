93c74e53e3453b118111f12aedc18e5940056f4b
royer2302 <royer2302@gmail.com>
2024-06-19 19:22:18 +0000

    about test update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,9 @@
   <div class="hello">
     <h2>{{ text }}</h2>
     <h1> <pre>{{ msg }}</pre></h1>
+    <div class="aboutContainer">
+      <p>Our company started in 2016</p>
+    </div>
     <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -74,4 +77,8 @@ pre {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 
   Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
+/* Suggested code may be subject to a license. Learn more: ~LicenseLog:3384507161. */
+.aboutContainer {
+  background-image: linear-gradient(45deg, rgba(255, 166, 0, 1) 14.7%, rgba(255, 99, 97, 1) 73%);  
+  }
 </style>
